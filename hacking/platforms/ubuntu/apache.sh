cceb7eae46362385aead256eb3dc8b64214abc5f
Martin Lambertz <martin@0x4d4c.xyz>
2016-05-09 21:59:15 +0200

    Add Apache config for Ubuntu 16.04
    It's the same as the 14.04 config actually.

--- hacking/platforms/ubuntu/apache.sh
+++ hacking/platforms/ubuntu/apache.sh
@@ -7,7 +7,7 @@ if [ ! -f /etc/apache2/cif.conf ]; then
     /bin/cp cif.conf /etc/apache2/
 fi
 
-if [ $VER == "12.04" ]; then
+if [ "$VER" == "12.04" ]; then
     cp /etc/apache2/sites-available/default-ssl /etc/apache2/sites-available/default-ssl.orig
     cp default-ssl /etc/apache2/sites-available
     a2dissite default
@@ -15,7 +15,7 @@ if [ $VER == "12.04" ]; then
     sed -i 's/^ServerTokens OS/ServerTokens Prod/' /etc/apache2/conf.d/security
     sed -i 's/^ServerSignature On/#ServerSignature On/' /etc/apache2/conf.d/security
     sed -i 's/^#ServerSignature Off/ServerSignature Off/' /etc/apache2/conf.d/security
-elif [ $VER == "14.04" ]; then
+elif [ "$VER" == "14.04" -o "$VER" == "16.04" ]; then
     cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.orig
     cp default-ssl /etc/apache2/sites-available/default-ssl.conf
     a2dissite 000-default.conf
